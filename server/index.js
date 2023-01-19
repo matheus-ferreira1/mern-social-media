61c59677fba622bf57c108ebbc32d42ba547c1b6
matheus-ferreira1 <matheus.fer33@gmail.com>
2023-01-18 21:49:15 -0300

    added users controllers

--- server/index.js
+++ server/index.js
@@ -10,6 +10,8 @@ import path from "path";
 import { fileURLToPath } from "url";
 
 import authRoutes from "./routes/auth.js";
+
+import userRoutes from "./routes/users.js";
 import { register } from "./controllers/auth.js";
 
 // configurations
@@ -45,6 +47,7 @@ app.post("/auth/register", upload.single("picture"), register);
 
 // ROUTES
 app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
 
 // MONGOOSE CONFIGURATION
 const PORT = process.env.PORT || 6001;
