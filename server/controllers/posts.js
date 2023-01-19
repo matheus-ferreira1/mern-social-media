fae2eadccaf5dc3e8f3e26a5a6ff567c9c481f93
matheus-ferreira1 <matheus.fer33@gmail.com>
2023-01-19 20:20:33 -0300

    added create post controller

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,6 +1,32 @@
-// import Post from '../models/Post.js'
+import Post from "../models/Post.js";
+import User from "../models/User.js";
 
-export const createPost = async (req, res) => {};
+export const createPost = async (req, res) => {
+  try {
+    const { userId, description, picturePath } = req.body;
+
+    const user = await Userer.findById(userId);
+
+    const newPost = new Post({
+      userId,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      location: user.location,
+      description,
+      userPicturePath: picturePath,
+      picturePath,
+      likes: {},
+      comments: [],
+    });
+
+    await newPost.save();
+
+    const post = await Post.find();
+    res.status(201).json(post);
+  } catch (err) {
+    res.status(409).json({ message: err.message });
+  }
+};
 
 export const getFeedPosts = async (req, res) => {};
 
