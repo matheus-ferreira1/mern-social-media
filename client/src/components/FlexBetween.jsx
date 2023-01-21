03b6b6240ffa830052c0db454f0257767aa6bc44
matheus-ferreira1 <matheus.fer33@gmail.com>
2023-01-21 12:35:20 -0300

    creation of flexbetween as component

---
+++ client/src/components/FlexBetween.jsx
@@ -0,0 +1,10 @@
+import { Box } from "@mui/material";
+import { styled } from "@mui/system";
+
+const FlexBetween = styled(Box)({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+});
+
+export default FlexBetween;
