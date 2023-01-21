8c48e5e60c638a20532226775e8bd75d37927011
matheus-ferreira1 <matheus.fer33@gmail.com>
2023-01-21 12:30:58 -0300

    color, theme, dark mode and styling setup

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App";
 
 import authReducer from "./state";
 import { configureStore } from "@reduxjs/toolkit";
-import { Provider } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
 import {
   persistStore,
   persistReducer,
